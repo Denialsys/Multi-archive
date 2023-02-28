dc775a5cc5e5275087473e64394188959c121fb7
Jenver <denialsys@gmail.com>
2023-02-28 15:29:11 +0800

    removed unnecessary LOC on setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
-from setuptools import setup, find_packages
-import codecs
+from setuptools import setup, find_package
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'For multiple compression and decompression'
