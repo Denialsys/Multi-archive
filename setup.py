c25df69fb784509c043c83f58b53d2cca8d4dad5
Jenver <denialsys@gmail.com>
2023-03-30 17:35:04 +0800

    update the version for the fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 
-VERSION = '0.1.0'
+VERSION = '0.1.3'
 DESCRIPTION = 'For multiple compression and decompression'
 LONG_DESCRIPTION = 'Package for programmatic archival or extraction of archive files with passwords'
 
